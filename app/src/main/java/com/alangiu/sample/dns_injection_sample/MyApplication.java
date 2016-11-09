c15cc62ea8c28c9846df072f312f51bf29cac47b
Antonio Langiu <antonio.langiu@studenti.polito.it>
2016-11-09 13:50:00 +0100

    extend the application class to easily share data between activiies

---
+++ app/src/main/java/com/alangiu/sample/dns_injection_sample/MyApplication.java
@@ -0,0 +1,15 @@
+package com.alangiu.sample.dns_injection_sample;
+
+import android.app.Application;
+
+import java.util.LinkedList;
+
+public class MyApplication extends Application {
+    public LinkedList<Test> testList;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        activeTests = new LinkedList<>();
+    }
+}
