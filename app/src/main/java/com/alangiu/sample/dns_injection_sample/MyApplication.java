4cc10fdb0723e1c7b399af3f70032d582c1ce4cd
Antonio Langiu <antonio.langiu@studenti.polito.it>
2016-11-09 18:38:12 +0100

    fix shared variable name

--- app/src/main/java/com/alangiu/sample/dns_injection_sample/MyApplication.java
+++ app/src/main/java/com/alangiu/sample/dns_injection_sample/MyApplication.java
@@ -10,6 +10,6 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        activeTests = new LinkedList<>();
+        testList = new LinkedList<>();
     }
 }
