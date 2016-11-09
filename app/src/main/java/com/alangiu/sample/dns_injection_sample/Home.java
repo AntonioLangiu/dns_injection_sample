e6bf62e9e95a13419df622492c6fe61e1f4f844b
Antonio Langiu <antonio.langiu@studenti.polito.it>
2016-11-09 11:18:51 +0100

    add mk initialization function

--- app/src/main/java/com/alangiu/sample/dns_injection_sample/Home.java
+++ app/src/main/java/com/alangiu/sample/dns_injection_sample/Home.java
@@ -3,11 +3,23 @@ package com.alangiu.sample.dns_injection_sample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.openobservatory.measurement_kit.android.LoadLibraryUtils;
+import org.openobservatory.measurement_kit.android.ResourceUtils;
+
 public class Home extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        initializeLibrary();
+    }
+
+    private void initializeLibrary() {
+        LoadLibraryUtils.load_measurement_kit();
+        ResourceUtils.copy_ca_bundle(this, R.raw.cacert);
+        ResourceUtils.copy_geoip(this, R.raw.geoip);
+        ResourceUtils.copy_geoip_asnum(this, R.raw.geoipasnum);
     }
 }
